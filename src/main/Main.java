766cc717cd05a3ea1b203952ff7e0cc19c73e498
urwinday <tregiven@gmail.com>
2017-12-05 20:44:50 +0400

    test git

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * urwinday
+ * 05.12.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.format("Hello %s !", args[0]);
+    }
+}
