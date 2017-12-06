1e673438ae2bf7717ea68fae715313a7df5242ec
urwinday <tregiven@gmail.com>
2017-12-07 02:49:03 +0400

    add Link

---
+++ src/com/urwinday/webapp/model/Link.java
@@ -0,0 +1,11 @@
+package com.urwinday.webapp.model;
+
+/**
+ * urwinday
+ * 07.12.2017.
+ */
+public class Link {
+    private String name;
+    private String url;
+
+}
---
+++ src/com/urwinday/webapp/model/Resume.java
@@ -0,0 +1,13 @@
+package com.urwinday.webapp.model;
+
+import java.util.Collection;
+
+/**
+ * urwinday
+ * 05.12.2017.
+ */
+public class Resume {
+    private String fullName;
+    private String location;
+
+}
