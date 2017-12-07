790c624f0b162b5e4e5fca1686d8d11ab9457a34
urwinday <tregiven@gmail.com>
2017-12-07 13:59:23 +0400

    implement equals

--- src/com/urwinday/webapp/model/Link.java
+++ src/com/urwinday/webapp/model/Link.java
@@ -5,11 +5,35 @@ package com.urwinday.webapp.model;
  * 07.12.2017.
  */
 public class Link {
-    private String name;
-    private String url;
+    private final String name;
+    private final String url;
 
     public Link(String name, String url) {
         this.name = name;
         this.url = url;
+    }
+
+    // конструктор копирования в замен метода clone();
+    public Link(Link link) {
+        this.name = link.name;
+        this.url = link.url;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Link link = (Link) o;
+
+        if (name != null ? !name.equals(link.name) : link.name != null) return false;
+        return url.equals(link.url);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + url.hashCode();
+        return result;
     }
 }
--- src/lesson02/Main.java
+++ src/lesson02/Main.java
@@ -1,8 +1,16 @@
 package lesson02;
 
+import com.urwinday.webapp.model.Link;
+
 /**
  * urwinday
  * 07.12.2017.
  */
 public class Main {
+    public static void main(String[] args) {
+        Link l1 = new Link("name", "url");
+        System.out.println(l1);
+        System.out.println(l1.getClass());
+
+    }
 }
