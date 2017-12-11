275dd6b5d132d3e5bfebad1fdb637ee147616529
urwinday <tregiven@gmail.com>
2017-12-11 20:22:26 +0400

    enhance ContactType

--- src/com/urwinday/webapp/model/ContactType.java
+++ src/com/urwinday/webapp/model/ContactType.java
@@ -5,10 +5,19 @@ package com.urwinday.webapp.model;
  * 08.12.2017.
  */
 public enum ContactType {
-    PHONE,
-    MOBILE,
-    HOME_PHONE,
-    SKYPE,
-    MAIL
+    PHONE("Тел."),
+    MOBILE("Мобильный"),
+    HOME_PHONE("Домашний телефон"),
+    SKYPE("Skype"),
+    MAIL("Почта");
 
+    private String title;
+
+    ContactType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
