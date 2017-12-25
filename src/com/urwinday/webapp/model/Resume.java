e697e4880f340072e4c05e861d25725e4e5273a2
urwinday <tregiven@gmail.com>
2017-12-25 20:20:20 +0400

    Add resume constructors

--- src/com/urwinday/webapp/model/Resume.java
+++ src/com/urwinday/webapp/model/Resume.java
@@ -1,7 +1,9 @@
 package com.urwinday.webapp.model;
 
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * urwinday
@@ -12,10 +14,15 @@ public class Resume {
     private String fullName;
     private String location;
     private String homePage;
-    private List<Contact> contact;
-    private List<Section> sections;
+    private List<Contact> contacts = new LinkedList<>();
+    private List<Section> sections = new LinkedList<>();
 
     public Resume(String fullName, String location) {
+        this(UUID.randomUUID().toString(), fullName, location);
+    }
+
+    public Resume(String uuid, String fullName, String location) {
+        this.uuid = uuid;
         this.fullName = fullName;
         this.location = location;
     }
@@ -30,8 +37,39 @@ public class Resume {
         return uuid.equals(resume.uuid);
     }
 
+    public void addSection(Section section) {
+        sections.add(section);
+    }
+    public void adddContact(Contact contact) {
+        contacts.add(contact);
+    }
+
     @Override
     public int hashCode() {
         return uuid.hashCode();
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getHomePage() {
+        return homePage;
+    }
+
+    public List<Contact> getContact() {
+        return contacts;
+    }
+
+    public List<Section> getSections() {
+        return sections;
     }
 }
