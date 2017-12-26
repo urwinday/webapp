10e2c088369df6ce3abb0648a03478a48fa07295
urwinday <tregiven@gmail.com>
2017-12-26 21:49:49 +0400

    Add JUnit

---
+++ test/com/urwinday/webapp/storage/ArrayStorageTest.java
@@ -0,0 +1,38 @@
+package com.urwinday.webapp.storage;
+
+import static org.junit.Assert.*;
+
+/**
+ * urwinday
+ * 26.12.2017.
+ */
+public class ArrayStorageTest {
+    @org.junit.Test
+    public void clear() throws Exception {
+    }
+
+    @org.junit.Test
+    public void save() throws Exception {
+    }
+
+    @org.junit.Test
+    public void update() throws Exception {
+    }
+
+    @org.junit.Test
+    public void load() throws Exception {
+    }
+
+    @org.junit.Test
+    public void delete() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getAllSorted() throws Exception {
+    }
+
+    @org.junit.Test
+    public void size() throws Exception {
+    }
+
+}
